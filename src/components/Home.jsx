e54158e4ca0933efa85bef273960b41fe3191c2b
jitender <cjatinder39@gmail.com>
2023-04-07 22:52:39 -0700

    completed the project

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,95 +1,123 @@
 import React from "react";
-import woman from "../img/gg.png";
 import styled from "styled-components";
+import Woman from "../img/gg.png";
+import AnimatedShapes from "./AnimatedShapes";
+
+const Container = styled.div`
+  height: calc(100vh - 50px);
+  display: flex;
+  padding: 20px;
+  @media only screen and (max-width: 480px) {
+    flex-direction: column;
+  }
+`;
+
+const Left = styled.div`
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  @media only screen and (max-width: 480px) {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const Title = styled.h1`
+  width: 60%;
+  font-size: 60px;
+  @media only screen and (max-width: 480px) {
+    width: 100%;
+    font-size: 50px;
+  }
+`;
+
+const Desc = styled.p`
+  width: 60%;
+  font-size: 20px;
+  margin-top: 20px;
+  @media only screen and (max-width: 480px) {
+    width: 100%;
+  }
+`;
+
+const Info = styled.div`
+  width: 60%;
+  margin-top: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  @media only screen and (max-width: 480px) {
+    width: 100%;
+    flex-direction: column;
+  }
+`;
+
+const Button = styled.button`
+  padding: 15px;
+  background-color: darkblue;
+  color: white;
+  border-radius: 10px;
+  font-weight: bold;
+  border: none;
+  letter-spacing: 2px;
+  cursor: pointer;
+  @media only screen and (max-width: 480px) {
+    margin-bottom: 20px;
+  }
+`;
+
+const Contact = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Phone = styled.span`
+  color: #f0667d;
+  font-weight: bold;
+`;
+
+const ContactText = styled.span`
+  color: gray;
+  margin-top: 5px;
+`;
+
+const Right = styled.div`
+  width: 40%;
+  @media only screen and (max-width: 480px) {
+    display: none;
+  }
+`;
+
+const Image = styled.img`
+  width: 100%;
+`;
+
 const Home = () => {
   return (
     <Container>
-      <Main>
-        <Left>
-          <H1>
-            Adventures in <br /> creative age
-          </H1>
-          <Para>
-            We believe that designing product and services in close partnership
-            with our clients in the only way to have a real impact on their
-            business
-          </Para>
-          <Detail>
-            <Button>START A PROJECT</Button>
-            <SpanDiv>
-              <Span1>Call Us +6239318097</Span1> <br />
-              <Span2>For Any Question or Concern</Span2>
-            </SpanDiv>
-          </Detail>
-        </Left>
-        <div className="right">
-          <Img src={woman} alt="hero image" />
-        </div>
-      </Main>
+      <Left>
+        <Title>Adventures in creative age</Title>
+        <Desc>
+          We believe that designing products and services in close partnership
+          with our clients is the only way to have a real impact on their
+          business.
+        </Desc>
+        <Info>
+          <Button>START A PROJECT</Button>
+          <Contact>
+            <Phone>Call Us (012) 345 - 6789</Phone>
+            <ContactText>For any question or concern</ContactText>
+          </Contact>
+        </Info>
+      </Left>
+      <Right>
+        <Image src={Woman} />
+      </Right>
+      <AnimatedShapes />
     </Container>
   );
 };
 
 export default Home;
-
-const Container = styled.div`
-  height: calc(100vh - 50px);
-  padding-left: 60px;
-`;
-
-const Main = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const Left = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 50%;
-`;
-
-const H1 = styled.h1`
-  font-size: 4rem;
-`;
-const Para = styled.p`
-  margin-top: 1rem;
-  font-size: 20px;
-  width: 70%;
-  color: #565656;
-`;
-
-const Detail = styled.div`
-  margin-top: 2rem;
-  display: flex;
-
-  gap: 3rem;
-`;
-
-const Button = styled.button`
-  padding: 10px 15px;
-  background-color: darkblue;
-  border: white;
-  border-radius: 10px;
-  color: white;
-  letter-spacing: 2px;
-  cursor: pointer;
-`;
-
-const SpanDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.2rem;
-`;
-
-const Span1 = styled.span`
-  color: crimson;
-`;
-
-const Span2 = styled.span`
-  color: gray;
-`;
-
-const Img = styled.img`
-  width: 31.4rem;
-`;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,82 +1,72 @@
 import React from "react";
 import styled from "styled-components";
+
+const Container = styled.div`
+  height: 50px;
+`;
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  width: 60%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+  text-decoration: underline crimson;
+`;
+
+const Menu = styled.ul`
+  display: flex;
+  list-style: none;
+
+  @media only screen and (max-width: 480px) {
+    display: none;
+  }
+`;
+
+const MenuItem = styled.li`
+  margin-right: 30px;
+  font-size: 20px;
+  font-weight: bold;
+  color: gray;
+`;
+
+const Button = styled.button`
+  border: 2px solid white;
+  padding: 10px 15px;
+  background-color: crimson;
+  color: white;
+  font-weight: bold;
+  border-radius: 10px;
+  cursor: pointer;
+`;
+
 const Navbar = () => {
   return (
     <Container>
-      <Left>
-        <Span>Agency</Span>
-        <Nav>
-          <Ul>
-            <Li>Home</Li>
-            <Li>Features</Li>
-            <Li>Services</Li>
-            <Li>Pricing</Li>
-            <Li>Contact</Li>
-          </Ul>
-        </Nav>
-      </Left>
-
-      <Right>
+      <Wrapper>
+        <Left>
+          <Logo>Agency</Logo>
+          <Menu>
+            <MenuItem>Home</MenuItem>
+            <MenuItem>Features</MenuItem>
+            <MenuItem>Services</MenuItem>
+            <MenuItem>Pricing</MenuItem>
+            <MenuItem>Contact</MenuItem>
+          </Menu>
+        </Left>
         <Button>JOIN TODAY</Button>
-      </Right>
+      </Wrapper>
     </Container>
   );
 };
 
 export default Navbar;
-
-const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 1rem 2rem;
-  height: 50px;
-  /* width: 100%; */
-`;
-
-const Left = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 60%;
-`;
-
-const Span = styled.span`
-  font-size: 2rem;
-  text-decoration: underline crimson 2px;
-`;
-
-const Nav = styled.nav`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-const Ul = styled.ul`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  gap: 2.5rem;
-  list-style: none;
-`;
-const Li = styled.li`
-  font-size: 1.2rem;
-  color: gray;
-`;
-
-const Right = styled.div`
-  width: 40%;
-  display: flex;
-  align-items: center;
-  justify-content: end;
-  overflow: hidden;
-`;
-
-const Button = styled.button`
-  outline: none;
-  padding: 0.5rem 1rem;
-  background-color: crimson;
-  color: white;
-  border: 1px solid white;
-  border-radius: 0.5rem;
-  cursor: pointer;
-`;
